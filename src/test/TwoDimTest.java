6020ad43bd89c9e54ca5b2716de4b01372382fe0
wcgzgj <zhangjia_good@126.com>
2020-12-10 16:49:32 +0800

    二维数组中的查找

---
+++ src/test/TwoDimTest.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ * @ClassName TwoDimTest
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2020/12/10 下午4:39
+ * @Version 1.0
+ **/
+public class TwoDimTest {
+    public static void main(String[] args) {
+        int[][] mix = new int[5][10];
+        int row = mix.length;
+        int col = mix[0].length;
+        System.out.println(row+"  "+col);
+    }
+}
