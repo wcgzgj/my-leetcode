b3880c0228ff7be15f3f427275a18c1a8e6d4db7
wcgzgj <zhangjia_good@126.com>
2021-01-10 21:18:29 +0800

    二叉树的层平均值

---
+++ src/leetcode/editor/cn/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,74 @@
+ //给定一个非空二叉树, 返回一个由每层节点平均值组成的数组。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//输出：[3, 14.5, 11]
+//解释：
+//第 0 层的平均值是 3 ,  第1层是 14.5 , 第2层是 11 。因此返回 [3, 14.5, 11] 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点值的范围在32位有符号整数范围内。 
+// 
+// Related Topics 树 
+// 👍 227 👎 0
+
+  
+  package leetcode.editor.cn;
+
+ import domain.TreeNode;
+
+ import java.util.ArrayList;
+ import java.util.LinkedList;
+ import java.util.List;
+ import java.util.Queue;
+
+ public class AverageOfLevelsInBinaryTree{
+      public static void main(String[] args) {
+           Solution solution = new AverageOfLevelsInBinaryTree().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        if (root==null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double count=0;
+            for (int i = 0; i < size; i++) {
+                TreeNode tmp = queue.poll();
+                count+=tmp.val;
+                if (tmp.left!=null) queue.offer(tmp.left);
+                if (tmp.right!=null) queue.offer(tmp.right);
+            }
+            res.add(count/size);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
