84ce55dcb86a08bf5541d6790de684787f269d1c
wcgzgj <zhangjia_good@126.com>
2021-06-25 15:54:11 +0800

    二维数组排序的方法

--- src/test/ArrayInitTest.java
+++ src/test/ArrayInitTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @ClassName ArrayInitTest
  * @Description 数组初始化测试
@@ -9,6 +12,23 @@ package test;
  **/
 public class ArrayInitTest {
     public static void main(String[] args) {
-
+        int[][] arr = new int[][]{
+                {2,3},
+                {1,4},
+                {4,1},
+                {5,8},
+                {6,2}};
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1]>o2[1]) {
+                    return 1;
+                }
+                return -1;
+            }
+        });
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
     }
 }
