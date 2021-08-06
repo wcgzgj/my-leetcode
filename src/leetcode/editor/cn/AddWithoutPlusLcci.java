76bcc63cd3a4f6bb447912cd03cbe654a519ada3
FARO_Z <zhangjia_good@126.com>
2021-08-06 18:17:32 +0800

    m

---
+++ src/leetcode/editor/cn/AddWithoutPlusLcci.java
@@ -0,0 +1,33 @@
+ //设计一个函数把两个数字相加。不得使用 + 或者其他算术运算符。 
+//
+// 示例: 
+//
+// 输入: a = 1, b = 1
+//输出: 2 
+//
+// 
+//
+// 提示： 
+//
+// 
+// a, b 均可能是负数或 0 
+// 结果不会溢出 32 位整数 
+// 
+// Related Topics 位运算 数学 
+// 👍 48 👎 0
+
+  package leetcode.editor.cn;
+  
+  public class AddWithoutPlusLcci{
+      public static void main(String[] args) {
+           Solution solution = new AddWithoutPlusLcci().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int add(int a, int b) {
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
