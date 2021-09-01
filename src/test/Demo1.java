c04c5ecaa430ce8562d1abb9fc572f999189860f
FARO_Z <zhangjia_good@126.com>
2021-09-01 10:44:13 +0800

    比较版本号

---
+++ src/test/Demo1.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ * @ClassName Demo1
+ * @Description TODO
+ * @Author faro_z
+ * @Date 2021/9/1 10:20 上午
+ * @Version 1.0
+ **/
+public class Demo1 {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i]=i+1;
+        }
+    }
+}
